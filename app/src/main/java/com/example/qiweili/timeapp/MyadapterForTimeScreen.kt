654a8cd7df7eb66ef9a94eb2257dbe782b422f82
liqiwei <liqiwei@iastate.edu>
2018-02-21 02:55:12 -0600

    Added weather and changed the wallpaper

--- app/src/main/java/com/example/qiweili/timeapp/MyadapterForTimeScreen.kt
+++ app/src/main/java/com/example/qiweili/timeapp/MyadapterForTimeScreen.kt
@@ -4,14 +4,14 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.cardview.view.*
-import java.util.*
 
 /**
  * Created by qiweili on 2018/2/18.
  */
-class MyadapterForTimeScreen(time : clocker) : RecyclerView.Adapter<TimeScreen.ViewHolder>() {
+class MyadapterForTimeScreen(time: clocker, cities: List<City>?) : RecyclerView.Adapter<TimeScreen.ViewHolder>() {
 
     val time = time
+    val cities = cities
     override fun getItemCount(): Int {
         return 2;
     }
@@ -25,21 +25,50 @@ class MyadapterForTimeScreen(time : clocker) : RecyclerView.Adapter<TimeScreen.V
     override fun onBindViewHolder(holder: TimeScreen.ViewHolder?, position: Int) {
         val chicago = time.time_chicago
         val beijing = time.time_china
-        if(position == 0){
-            holder?.view?.timezone?.text = "Chicago"
+        if (position == 0) {
+            holder?.view?.timezone?.text = "Ames ${cities?.get(position)?.main?.getMinHighTemp()}"
             holder?.view?.time?.text = chicago
-            if (time.chicago_daytime){
-                holder?.view?.cardview_background?.setBackgroundResource(R.drawable.daytime)
-            }else{
-                holder?.view?.cardview_background?.setBackgroundResource(R.drawable.nightime)
+            if (time.chicago_daytime) {
+                holder?.view?.cardview_background?.setBackgroundResource(R.drawable.daylight2)
+                if (cities?.get(position)?.weather?.get(0)?.returnWeather() == "Sun") {
+                    holder?.view?.cardview?.sun_or_moon?.setBackgroundResource(R.drawable.sun)
+                } else {
+                    holder?.view?.cardview?.sun_or_moon?.setBackgroundResource(R.drawable.rain)
+                }
+
+            } else {
+                holder?.view?.cardview_background?.setBackgroundResource(R.drawable.night2)
+                if (cities?.get(position)?.weather?.get(0)?.returnWeather() == "Sun") {
+                    holder?.view?.cardview?.sun_or_moon?.setBackgroundResource(R.drawable.moon)
+                } else {
+                    holder?.view?.cardview?.sun_or_moon?.setBackgroundResource(R.drawable.rain)
+                }
             }
-        }else{
-            holder?.view?.timezone?.text = "Beijing"
+        } else {
+            holder?.view?.timezone?.text = "Shenzhen ${cities?.get(position)?.main?.getMinHighTemp()}"
             holder?.view?.time?.text = beijing
-            if (time.china_daytime){
-                holder?.view?.cardview_background?.setBackgroundResource(R.drawable.daytime)
-            }else{
-                holder?.view?.cardview_background?.setBackgroundResource(R.drawable.nightime)
+            if (time.china_daytime) {
+                holder?.view?.cardview_background?.setBackgroundResource(R.drawable.daylight1)
+                try {
+                    if (cities?.get(position)?.weather?.get(position)?.returnWeather() == "Sun") {
+                        holder?.view?.cardview?.sun_or_moon?.setBackgroundResource(R.drawable.sun)
+                    } else {
+                        holder?.view?.cardview?.sun_or_moon?.setBackgroundResource(R.drawable.rain)
+                    }
+                } catch (e: Exception) {
+                    println(e)
+                }
+            } else {
+                holder?.view?.cardview_background?.setBackgroundResource(R.drawable.night1)
+                try {
+                    if (cities?.get(position)?.weather?.get(0)?.returnWeather() == "Sun") {
+                        holder?.view?.cardview?.sun_or_moon?.setBackgroundResource(R.drawable.moon)
+                    } else {
+                        holder?.view?.cardview?.sun_or_moon?.setBackgroundResource(R.drawable.rain)
+                    }
+                } catch (e: Exception) {
+                    println(e)
+                }
             }
 
         }
--- app/src/main/java/com/example/qiweili/timeapp/TimeScreen.kt
+++ app/src/main/java/com/example/qiweili/timeapp/TimeScreen.kt
@@ -1,5 +1,6 @@
 package com.example.qiweili.timeapp
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -11,7 +12,9 @@ import kotlinx.android.synthetic.main.activity_time_screen.*
 import kotlinx.android.synthetic.main.activity_time_screen.view.*
 import java.time.LocalTime
 import android.widget.TextView
-
+import com.google.gson.GsonBuilder
+import okhttp3.*
+import java.io.IOException
 
 
 class TimeScreen : AppCompatActivity() {
@@ -19,10 +22,9 @@ class TimeScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_time_screen)
-
         countries.layoutManager = LinearLayoutManager(countries.context)
-        countries.adapter = MyadapterForTimeScreen(clocker)
-        updateTime()
+        getWeather(this)
+
     }
 
     fun updateTime() {
@@ -37,7 +39,37 @@ class TimeScreen : AppCompatActivity() {
         timerHandler.post(updater)
     }
 
+    fun getWeather(context : Context){
+        var apiwebsite = "http://api.openweathermap.org/data/2.5/group?id="
+        val shenzhen = 1795565
+        val ames = 4846834
+        val apikey = "&APPID=f0fd2b714837ffbd8fe592111884247f"
 
+        val client = OkHttpClient()
+        val request = Request.Builder().url(apiwebsite + ames+ "," + shenzhen + apikey).build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call?, e: IOException?) {
+                println("Error")
+                runOnUiThread {
+                    countries.adapter = MyadapterForTimeScreen(clocker,null)
+                    updateTime()
+                }
+            }
+
+            override fun onResponse(call: Call?, response: Response?) {
+                val body = response?.body()?.string()
+                val gson = GsonBuilder().create()
+                val list = gson.fromJson(body, listCities::class.java)
+                runOnUiThread {
+                    countries.adapter = MyadapterForTimeScreen(clocker,list?.list)
+                    updateTime()
+                }
+            }
+        })
+
+
+    }
 
     class ViewHolder(val view: View) : RecyclerView.ViewHolder(view){
 
